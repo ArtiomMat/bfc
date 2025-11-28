c35182e8333e5f3887ea65e8654e5ba60e0f9f82
Your Name <you@example.com>
2025-11-28 18:27:14 +0200

    AAAA

--- bfc.c
+++ bfc.c
@@ -46,16 +46,39 @@ typedef struct Op {
 } Op;
 
 /*
- * The same size as `text`.
- *
- * The purpose of this global is to mark `[` and `]` if they don't signify
- * ops but rather delimiters for previous `]` and `[` respectively.
- *
- * For example, if `text[10]` has `[` and then in `text[15]` there is `]`
- * that then `G_DELIMITER_BRACKETS[15]` is marked with `1` to show that it's
- * only a delimiter.
+ * Represents an extended version of the source code,
+ * with members to easen lexing.
  */
-char* G_DELIMITER_BRACKETS = NULL;
+typedef struct{
+  const char* text;
+
+  /*
+   * The size as `len`.
+   *
+   * The purpose of this global is to mark `[` and `]` if they don't signify
+   * ops but rather delimiters for previous `]` and `[` respectively.
+   *
+   * For example, if `text[10]` has `[` and then in `text[15]` there is `]`
+   * that then `delimiter_brackets[15]` is marked with `1` to show that it's
+   * only a delimiter.
+   */
+  char* delimiter_brackets;
+
+  /*
+   * Cache it because we already calculate it anyway.
+   *
+   * Length not including null terminator.
+   */
+  int len;
+
+  /*
+   * Index within text.
+   */
+  int i;
+} Source;
+
+/* Set to 1 if there was an error somewhere in the pipeline. */
+static int G_ERROR = 0;
 
 static void log(LogLevel level, const char* fmt, va_list args){
   printf("%i: ", level);
@@ -72,74 +95,133 @@ static void log_error(const char* fmt, ...) {
 
 static OpType op_type_from_c(const char c) {
   switch (c) {
-    case '+':
-    case '-':
-      return OP_MUTATE;
-    case '>':
-    case '<':
-      return OP_MOVE;
-    case ',':
-      return OP_INPUT;
-    case '.':
-      return OP_PRINT;
-    case '[':
-      return OP_IF_0;
-    case ']':
-      return OP_IF_NOT_0;
-    default:
-      return OP_SKIP;
+  case '+':
+  case '-':
+    return OP_MUTATE;
+  case '>':
+  case '<':
+    return OP_MOVE;
+  case ',':
+    return OP_INPUT;
+  case '.':
+    return OP_PRINT;
+  case '[':
+    return OP_IF_0;
+  case ']':
+    return OP_IF_NOT_0;
+  default:
+    return OP_SKIP;
   }
 }
 
 /*
- * `op` is modified according to `c` and its `OpType`.
+ * Analyzes the current character in `src->text` and updates `op` for that character.
+ *
+ * Modifies `src->i` as seen fit, preparing for the next `update_op_from_c()`.
  *
  * If `op->type` is not `OP_INVALID` and `c` is not of the same type,
- * this is an error.
+ * we avoid lexing it because a new `Op` needs to be created.
  *
- * Returns OP_INVALID on error, otherwise the `OpType` of `c`. 
- * This is done so the caller compares the return value with `op->type`
- * and knows if this is the last `c`.
+ * Returns if above calling code should break and start lexing a new
+ * `Op`.
  */
-static OpType update_op_from_c(const char c, Op* op) {
+static OpType update_op_from_c(Source* src, Op* op) {
+  const char c = src->text[src->i];
   OpType type = op_type_from_c(c);
 
+  /* First time updating. */
   if (OP_INVALID == op->type) {
+    assert(0 == op->n); /* Must be reset if OP_INVALID. */
 
+    switch (type) {
+    case OP_IF_NOT_0:
+    case OP_IF_0:
+      /* TODO: Implement marking delimiters and such */
+      break;
+
+    case OP_SKIP:
+    case OP_PRINT:
+    case OP_INPUT:
+    case OP_MUTATE:
+    case OP_MOVE:
+      op->n = 1;
+      break;
+
+    default:
+      assert(0); /* Unknown OP. */
+      break;
+    }
+
+    op->type = type;
+
+    goto _done;
   }
+
+  if (type != op->type) {
+    return 1; /* Avoid lexing and notify that should break */
+  }
+
+  switch (type) {
+  case OP_MUTATE:
+    assert('+' == c || '-' == c);
+    op->n += c == '+' ? 1 : -1;
+    break;
+
+  case OP_MOVE:
+    assert('>' == c || '<' == c);
+    op->n += c == '>' ? 1 : -1;
+    break;
+
+  case OP_INPUT:
+  case OP_PRINT:
+  case OP_SKIP:
+    ++op->n;
+    break;
+
+  case OP_IF_0:
+  case OP_IF_NOT_0:
+    assert(0); /* Should not have gotten here with the OP_IF_* */
+    break;
+  default:
+    assert(0); /* Unknown OP. */
+    break;
+  }
+
+_done:
+  ++src->i;
+  return 0;
 }
 
 /*
- * `i_ptr` is a pointer to the current index within `text` it is modified
- * as seen fit, and prepared for the next `tokenize_one_op()`.
+ * Analyzes `src->text` and constructs a full `Op` from as many related characters
+ * as possible. For example `++----` would group into an `OP_MUTATE` with `n=-2`.
+ *
+ * Modifies `src->i` as seen fit, preparing for the next `tokenize_one_op()`.
  *
  * Returns NULL if there was an error. `*i_ptr` will point to the problematic char.
  */
-static Op* tokenize_one_op(const char* text, int* i_ptr) {
+static Op* tokenize_one_op(Source* src) {
   Op* op = malloc(sizeof (Op));
-  int i = *i_ptr;
 
   op->next = NULL;
   op->type = OP_INVALID;
   op->n = 0;
 
-  for (i = *i_ptr; text[i]; ++i) {
-    const OpType type = update_op_from_c(text[i], op);
+  for (/* Already initialized */; src->i < src->len; /* Inside */) {
+    int should_break = update_op_from_c(src, op);
     
-    if (OP_INVALID == type) {
+    if (G_ERROR) {
       goto _failure;
-    } else if (op->type != type) {
-      /* Time to stop for this op */
+    }
+
+    if (should_break) {
       break;
     }
   }
 
-  *i_ptr = i;
   return op;
 
 _failure:
-  *i_ptr = i;
-
   if (op) {
     free(op);
   }
@@ -149,11 +231,10 @@ _failure:
 /*
  * Returns NULL if there was an error.
  */
-static Op* tokenize(const char* text) {
+static Op* tokenize(Source* src) {
   Op* first_op = NULL;
   Op* last_op = NULL; /* To know from where to push the next */
   Op* current_op = NULL; /* For iteration */
-  int i = 0;
 
 _failure:
   if (first_op) {
@@ -164,6 +245,19 @@ _failure:
     }
   }
   return NULL;
+}
+
+static Source source_from_text(const char* text) {
+  Source source;
+
+  assert(text);
+
+  source.text = text;
+  source.len = strlen(text);
+  source.i = 0;
+  source.delimiter_brackets = calloc(source.len, sizeof(*source.delimiter_brackets));
+
+  return source;
 }
 
 int main(const int argc, const char** argv) {
